3aa846df8e678fa38c8cf791bb84dde03fc65c4f
Caio Malvezzi <malvezzi.dev@gmail.com>
2025-10-04 11:46:58 -0300

    feat: database structure and docker compose

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,20 @@
+import { fastify } from 'fastify'
+import fastifyCors from '@fastify/cors'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createAccount } from './routes/auth/create-accout'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors)
+app.register(createAccount)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running')
+})
