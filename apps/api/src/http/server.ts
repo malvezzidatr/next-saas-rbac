fce0123248fc6e4bc0052cac2ab60a08a291a055
Caio Malvezzi <malvezzi.dev@gmail.com>
2025-10-13 16:30:14 -0300

    feat: 'including the JWT token'

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,6 +1,7 @@
 import { fastify } from 'fastify'
 import fastifyCors from '@fastify/cors'
 import fastifySwagger from '@fastify/swagger'
+import fastifyJWT from '@fastify/jwt'
 import fastifySwaggerUI from '@fastify/swagger-ui'
 import {
   jsonSchemaTransform,
@@ -9,6 +10,7 @@ import {
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -31,8 +33,13 @@ app.register(fastifySwaggerUI, {
   routePrefix: '/docs',
 })
 
+app.register(fastifyJWT, {
+  secret: 'my-jwt-secret',
+})
+
 app.register(fastifyCors)
 app.register(createAccount)
+app.register(authenticateWithPassword)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP server running')
